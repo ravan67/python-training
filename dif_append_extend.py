8feaa41c947bf61276bf2c1af32f95cdc7c9281a
ravan67 <114069238+ravan67@users.noreply.github.com>
2022-11-24 10:03:13 +0530

    Delete dif_append_extend.py

--- dif_append_extend.py
+++
@@ -1,8 +0,0 @@
-# append(): to add element in existing list
-# extend(): to add mtltiple element in existing list
-
-list = [1,2,3,4,5]
-list.append(6)
-print(list)
-list.extend([7,8,9])
-print(list)
