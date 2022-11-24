e2f0bd6303c2bd220c403d6325889314f42ef52b
ravan67 <114069238+ravan67@users.noreply.github.com>
2022-11-24 09:59:46 +0530

    Delete check_mult_key.py

--- check_mult_key.py
+++
@@ -1,8 +0,0 @@
-student = {
-  'name': 'ravan',
-  'class': '12',
-  'roll_id': '67'
-}
-print(student.keys() >= {'class', 'name'})
-print(student.keys() >= {'name', 'ravan'})
-print(student.keys() >= {'roll_id', 'name'})
